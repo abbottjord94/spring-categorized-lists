8804cc157cd60658fc735c456710accb78a63d99
Jordan Abbott <abbottjord94@gmail.com>
2022-05-18 12:56:53 -0700

    Documented functions that were already completed

--- src/main/java/com/example/spring/shoppinglist/ApplicationClasses/MongoDatabaseController.java
+++ src/main/java/com/example/spring/shoppinglist/ApplicationClasses/MongoDatabaseController.java
@@ -15,13 +15,24 @@ public class MongoDatabaseController {
         listCollection = db.getCollection("listItems");
     }
 
+    /*
+        createList(String _listName)
+            -Creates a list in the MongoDB database with the listName field specified by the _listName parameter.
+
+            Returns the MongoDB Document that was created
+     */
     public Document createList(String _listName) {
         System.out.println("createList" + _listName);
         Document listToCreate = new ItemList(_listName).toDocument();
         listCollection.insertOne(listToCreate);
         return listToCreate;
     }
+    /*
+        String getListByName(String _listName)
+            -Returns a list as a JSON-formatted string with the listName field specified by the _listName parameter. If no list is found, the createList() function is called.
 
+            Returns the JSON-formatted string of the list specified by the _listName parameter.
+     */
     public String getListByName(String _listName) {
         System.out.println("getListByName: " + _listName);
         FindIterable listDocQueryResult = listCollection.find(Filters.eq("listName", _listName));
@@ -35,18 +46,31 @@ public class MongoDatabaseController {
         }
     }
 
+    /*
+        addCategoryToList(String _listName, Category _category)
+            -Adds a Category _category to the list specified by _listName in the MongoDB database
+     */
     public void addCategoryToList(String _listName, Category _category) {
         listCollection.updateOne(Filters.eq("listName", _listName), new Document("$set", new Document("categories." + _category.getCategoryName(), _category.toDocument())));
     }
-
+    /*
+        removeCategoryFromList(String _listName, Category _category)
+            -Removes a Category _category to the list specified by _listName from the MongoDB database
+     */
     public void removeCategoryFromList(String _listName, Category _category) {
         listCollection.updateOne(Filters.eq("listName", _listName), new Document("$unset", new Document("categories." + _category.getCategoryName(), new Document())));
     }
-
+    /*
+        addItemToList(String _listName, String _categoryName, Document _item)
+            -Adds an Item _item in the Category _category to the list specified by _listName in the MongoDB database
+     */
     public void addItemToList(String _listName, String _categoryName, Document _item) {
         listCollection.updateOne(Filters.eq("listName", _listName), new Document("$push", new Document("categories." + _categoryName + ".items", _item)));
     }
-
+    /*
+        removeItemFromList(String _listName, String _categoryName, Document _item)
+            -Removes an Item _item in the Category _category from the list specified by _listName in the MongoDB database
+     */
     public void removeItemFromList(String _listName, String _categoryName, Document _item) {
         listCollection.updateOne(Filters.eq("listName", _listName), new Document("$pull", new Document("categories." + _categoryName + ".items", _item)));
     }
--- src/main/java/com/example/spring/shoppinglist/ApplicationClasses/ShoppingListRestController.java
+++ src/main/java/com/example/spring/shoppinglist/ApplicationClasses/ShoppingListRestController.java
@@ -18,6 +18,13 @@ public class ShoppingListRestController {
         mongoDatabaseController = new MongoDatabaseController();
     }
 
+    /*
+        /getList endpoint (POST)
+        Returns a list requested by the user
+
+        Data accepted:
+            -listName (JSON string)
+     */
     @PostMapping(value = "/getList",
                 consumes = MediaType.APPLICATION_JSON_VALUE,
                 produces = MediaType.APPLICATION_JSON_VALUE)
@@ -26,30 +33,67 @@ public class ShoppingListRestController {
         String jsonList = mongoDatabaseController.getListByName(_requestData.get("listname"));
         return jsonList;
     }
+    /*
+        /removeItems endpoint (POST)
+        Removes items from the list as requested by the user, and then returns the list after removal.
 
-    @PostMapping("/removeItems")
+        Data accepted:
+            -listName (JSON string)
+            -items(JSON array of Objects {category: categoryName, item: itemName} )
+     */
+    @PostMapping(value = "/removeItems",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     String removeItems(@RequestBody Map<String, String> _requestData) {
 
-        return "Remove Items";
+        String jsonList = mongoDatabaseController.getListByName(_requestData.get("listname"));
+        return jsonList;
     }
+    /*
+    /createCategory endpoint (POST)
+    Creates a category as specified by the user, then returns the list after adding the new category
 
-    @PostMapping("/createCategory")
+    Data accepted:
+        -listName (JSON string)
+        -category (JSON string)
+ */
+    @PostMapping(value = "/createCategory",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody String createCategory(@RequestBody Map<String, String> _requestData) {
         System.out.println("CreateCategory: " + _requestData.get("category"));
         mongoDatabaseController.addCategoryToList(_requestData.get("listname"), new Category(_requestData.get("category")));
         String jsonList = mongoDatabaseController.getListByName(_requestData.get("listname"));
         return jsonList;
     }
+    /*
+        /addItem endpoint (POST)
+        Adds items as specified by the user, and then returns the list with the items added
 
-    @PostMapping("/addItem")
+        Data accepted:
+            -listName (JSON string)
+            -itemName (JSON string)
+     */
+    @PostMapping(value = "/addItem",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     String addItem(@RequestBody Map<String, String> _requestData) {
         System.out.println("AddItem: " + _requestData.get("item"));
         mongoDatabaseController.addItemToList(_requestData.get("listname"), _requestData.get("category"), (new ListItem(_requestData.get("item"), 1.0f).toDocument()));
         String jsonList = mongoDatabaseController.getListByName(_requestData.get("listname"));
         return jsonList;
     }
+    /*
+        /removeCategory endpoint (POST)
+        Removes a category specified by the user, and then returns the list with the category removed
 
-    @PostMapping("/removeCategory")
+        Data accepted:
+            -listName (JSON string)
+            -category (JSON string)
+     */
+    @PostMapping(value = "/removeCategory",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     String removeCategory(@RequestBody Map<String, String> _requestData) {
         System.out.println("RemoveCategory: " + _requestData.get("category"));
         mongoDatabaseController.removeCategoryFromList(_requestData.get("listname"), new Category(_requestData.get("category")));
